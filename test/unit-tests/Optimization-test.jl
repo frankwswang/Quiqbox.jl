fcaca80c4ca013db97e52c73ba20614072d1ceeb
frankwswang <frank.wang.w.s@gmail.com>
2021-09-01 19:57:58 -0400

    Adjust testing error threshold.

--- test/unit-tests/Optimization-test.jl
+++ test/unit-tests/Optimization-test.jl
@@ -4,7 +4,8 @@ using Suppressor: @suppress_out
 
 @testset "Optimization.jl" begin
 
-errorThreshold = 1e-12
+errorThreshold1 = 1e-10
+errorThreshold2 = 1e-6
 
 # Floating basis set
 nucCoords = [[-0.7,0.0,0.0], [0.7,0.0,0.0]]
@@ -20,17 +21,17 @@ local Es1L, pars1L, grads1L
     Es1L, pars1L, grads1L = optimizeParams!(bs1, pars1, mol, nucCoords, maxSteps = 200)
 end
 
-E_t1 = -1.7756825544202863
-par_t1 = [1.3316366727266504,  0.3118586356968696, 0.45479844661739155, 
-          0.6626439317888224, -0.6866294552929423, 0.6866294671212336, 
-          0.0, 0.0, 0.0, 0.0]
-grad_t1 = [-0.12518827510873726,  0.017527948869488608, -0.10779722877906275, 
-            0.07398545409754818, -0.056469017809637645,  0.05648285438029821, 
+E_t1 = -1.775682554420
+par_t1 =  [ 1.331636672727,  0.311858635697,  0.454798446617, 
+            0.662643931789, -0.686629455293,  0.686629467121, 
+            0.0, 0.0, 0.0, 0.0]
+grad_t1 = [-0.125188275109,  0.017527948869, -0.107797228779, 
+            0.073985454098, -0.056469017810,  0.056482854380, 
             0.0, 0.0, 0.0, 0.0]
 
-@test isapprox(Es1L[end], E_t1, atol=errorThreshold)
-@test isapprox(pars1L[end, :], par_t1, atol=errorThreshold)
-@test isapprox(grads1L[end, :], grad_t1, atol=errorThreshold)
+@test isapprox(Es1L[end], E_t1, atol=errorThreshold1)
+@test isapprox(pars1L[end, :], par_t1, atol=errorThreshold1)
+@test isapprox(grads1L[end, :], grad_t1, atol=errorThreshold1)
 
 
 # Grid-based basis set
@@ -42,15 +43,15 @@ pars2 = uniqueParams!(bs2, filterMapping=true)[[1,3]]
 
 local Es2L, pars2L, grads2L
 @suppress_out begin
-    Es2L, pars2L, grads2L = optimizeParams!(bs2, pars2, mol, nucCoords, maxSteps = 200)
+    Es2L, pars2L, grads2L = optimizeParams!(bs2, pars2, mol, nucCoords, maxSteps = 100)
 end
 
-E_t2 = -1.6227282934931644
-par_t2  = [0.49801096561597613, 1.408314680969665]
-grad_t2 = [0.4557364086913408, 0.32317362845855635]
+E_t2 = -1.60187771
+par_t2  = [0.56510208, 1.44471987]
+grad_t2 = [0.92745609, 0.42017915]
 
-@test isapprox(Es2L[end], E_t2, atol=errorThreshold)
-@test isapprox(pars2L[end, :], par_t2, atol=errorThreshold)
-@test isapprox(grads2L[end, :], grad_t2, atol=errorThreshold)
+@test isapprox(Es2L[end], E_t2, atol=errorThreshold2)
+@test isapprox(pars2L[end, :], par_t2, atol=errorThreshold2)
+@test isapprox(grads2L[end, :], grad_t2, atol=errorThreshold2)
 
 end
