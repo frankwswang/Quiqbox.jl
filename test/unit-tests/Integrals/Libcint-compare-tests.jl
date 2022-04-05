561da880b8cfd6a123741c8eeebfe1a71e88514a
frankwswang <frank.wang.w.s@gmail.com>
2022-04-05 17:07:04 +0800

    Code formatting.

--- test/unit-tests/Integrals/Libcint-compare-tests.jl
+++ test/unit-tests/Integrals/Libcint-compare-tests.jl
@@ -5,7 +5,7 @@ using Quiqbox: BasisFuncTexts, ElementNames, BasisSetNames
 include("../../../test/test-functions/Shared.jl")
 include("../../../test/test-functions/Libcint/Libcint.jl")
 
-@testset "Integral engine Quiqbox VS Libcint tests" begin
+@testset "Quiqbox and Libcint Gaussian integral tests" begin
 
 atms = map(1:8) do i
     list = findall(x->x!==nothing, BasisFuncTexts[i])
@@ -19,13 +19,13 @@ errT = 1e-12
 oFilter(f) = bs-> f(filter(i->typeof(i).parameters[1]<2, bs))
 
 fs1 = (oFilter(overlaps), 
-        oFilter(elecKinetics), 
-        oFilter(x->nucAttractions(x, nuc, nucCoords)), 
-        oFilter(eeInteractions))
+       oFilter(elecKinetics), 
+       oFilter(x->nucAttractions(x, nuc, nucCoords)), 
+       oFilter(eeInteractions))
 fs2 = (oFilter(overlapsLibcint), 
-        oFilter(elecKineticsLibcint), 
-        oFilter(x->nucAttractionsLibcint(x, nuc, nucCoords)), 
-        oFilter(eeInteractionsLibcint))
+       oFilter(elecKineticsLibcint), 
+       oFilter(x->nucAttractionsLibcint(x, nuc, nucCoords)), 
+       oFilter(eeInteractionsLibcint))
 fns = (:overlaps, :elecKinetics, :nucAttractions, :eeInteractions)
 
 for (f1, f2, fn) in zip(fs1, fs2, fns), pair in zip(BasisSetNames, atms)
