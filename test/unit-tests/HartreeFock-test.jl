c71d0f3e6db59a53ba6aa597dd5c53be070d183c
frankwswang <41162655+frankwswang@users.noreply.github.com>
2022-05-28 21:28:44 +0800

    Loosened test constraints.

--- test/unit-tests/HartreeFock-test.jl
+++ test/unit-tests/HartreeFock-test.jl
@@ -5,7 +5,7 @@ using Suppressor: @suppress_out
 @testset "HartreeFock.jl" begin
 
 errorThreshold1 = 1e-8
-errorThreshold2 = 5e-5
+errorThreshold2 = 1e-4
 errorThreshold3 = 5e-4
 
 nucCoords = [[-0.7,0.0,0.0], [0.7,0.0,0.0], [0.0, 0.0, 0.0]]
