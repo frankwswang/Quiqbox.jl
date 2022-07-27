678fa85f49cd5dc2a4fc8bfc2525a5dd109d738f
frankwswang <frank.ws.wang@gmail.com>
2022-07-27 00:13:28 -0400

    Added tests for `getCompositeIntCore`.

--- test/unit-tests/Integrals/Core-test.jl
+++ test/unit-tests/Integrals/Core-test.jl
@@ -3,6 +3,8 @@ using Quiqbox
 using Quiqbox: Fγ
 using QuadGK: quadgk
 
+include("../../../test/test-functions/Shared.jl")
+
 @testset "Core.jl tests" begin
 
 tolerance1 = 1e-8
@@ -24,5 +26,28 @@ bs_bf_bfs_bfm = [b1, bfm1, b2..., bfm2]
 
 @test try eeInteractions(bs_bf_bfs_bfm); true catch; false end
 @test try coreH(bs_bf_bfs_bfm, nuc, nucCoords); true catch; false end
+
+# function getCompositeIntCore
+tolerance3 = 1e-15
+for i in Quiqbox.SubshellNames[2:4]
+    bfs1 = genBasisFunc(rand(3), (rand(0.5:0.1:2.0, 2), rand(-0.5:0.1:0.5, 2)), "S")
+    bfs2 = genBasisFunc(rand(3), (rand(0.5:0.1:2.0, 2), rand(-0.5:0.1:0.5, 2)), "P")
+    bfs3 = genBasisFunc(rand(3), (rand(0.5:0.1:2.0, 2), rand(-0.5:0.1:0.5, 2)), i)
+    bs1 = [bfs1, bfs2, bfs3]
+    bs2 = [bfs1, bfs2[:]..., bfs3[:]...]
+    bss = (bs1, bs2)
+
+    S1, S2 = overlaps.(bss)
+    compr2Arrays3((S1=S1, S2=S2), tolerance3, true)
+
+    T1, T2 = eKinetics.(bss)
+    compr2Arrays3((T1=T1, T2=T2), tolerance3, true)
+
+    V1, V2 = neAttractions.(bss, Ref(nuc), Ref(nucCoords))
+    compr2Arrays3((V1=V1, V2=V2), tolerance3, true)
+
+    eeI1, eeI2 = eeInteractions.(bss)
+    compr2Arrays3((eeI1=eeI1, eeI2=eeI2), tolerance3)
+end
 
 end
