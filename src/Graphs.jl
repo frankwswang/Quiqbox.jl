9de3438971c32173f8e52604ee25a5a946498ab2
frankwswang <41162655+frankwswang@users.noreply.github.com>
2024-07-04 15:00:44 -0400

    Added `bias` to account for `ParamNode` offset.

--- src/Graphs.jl
+++ src/Graphs.jl
@@ -15,43 +15,10 @@ struct LayerNode{T, F, N, I<:AbstractArray{<:TreeNode{T}, N}} <: TreeNode{T, F, 
     connect::StableReduce{T, F}
     child::I
     marker::Symbol
+    bias::T
 end
 
 
-# function compressGraphNodeV1(tn::Quiqbox.ValueNode{T}) where {T}
-#     StableReduce(Vector{T}, (::AbstractVector{T})->tn.val)
-# end
-
-# function compressGraphNodeV1(tn::Quiqbox.IndexNode{T}) where {T}
-#     StableReduce(Vector{T}, (x::AbstractVector{T})->x[tn.idx])
-# end
-
-# function compressGraphNodeV1(tn::LayerNode{T}) where {T}
-#     let fs=map(compressGraphNodeV1, tn.child), connect=tn.connect
-#         # function (x::AbstractVector{T})
-#         #     connect.f(T[f(x) for f in fs])
-#         # end
-#         StableReduce(Vector{T}, (x::AbstractVector{T})->connect([f(x) for f in fs]))
-#     end
-# end
-
-# function compressGraphNodeV2(tn::Quiqbox.ValueNode{T}) where {T}
-#     (::AbstractVector{T})->tn.val
-# end
-
-# function compressGraphNodeV2(tn::Quiqbox.IndexNode{T}) where {T}
-#     (x::AbstractVector{T})->x[tn.idx]
-# end
-
-# function compressGraphNodeV2(tn::LayerNode{T}) where {T}
-#     let fs=map(compressGraphNodeV2, tn.child), connect=tn.connect
-#         function (x::AbstractVector{T})
-#             connect(T[f(x) for f in fs])
-#         end
-#     end
-# end
-
-# Best version
 function compressGraphNode(tn::ValueNode{T}) where {T}
     (::AbstractVector{T})->tn.val
 end
@@ -64,25 +31,26 @@ function compressGraphNode(tn::LayerNode{T}) where {T}
     fArr = map(compressGraphNode, tn.child)
     dim = size(fArr)
     fs = Tuple(fArr)
-    let fs=fs, dim=dim, connect=tn.connect
+    let fs=fs, dim=dim, connect=tn.connect, b=tn.bias
         function (x::AbstractVector{T})
-            fVals = collect(map(fs) do f; f(x) end)
-            reshape(fVals, dim) |> connect
+            fVals = collect(map(f->f(x), fs))
+            connect( reshape(fVals, dim) ) + b
         end
     end
 end
+
 
 computeTreeGraph(::AbstractVector{T}, tn::ValueNode{T}) where {T} = tn.val
 
 computeTreeGraph(data::AbstractVector{T}, tn::IndexNode{T}) where {T} = data[tn.idx]
 
-function computeTreeGraph(data::AbstractVector{T}, rn::LayerNode{T}) where {T}
-    broadcast(computeTreeGraph, Ref(data), rn.child) |> rn.connect
+function computeTreeGraph(data::AbstractVector{T}, ln::LayerNode{T}) where {T}
+    ln.connect( broadcast(computeTreeGraph, Ref(data), ln.child) ) + ln.bias
 end
 
 # This is necessary for certain AD library to work properly/efficiently as of Julia 1.10
-function computeTreeGraph(data::AbstractVector{T}, rn::LayerNode{T, <:Any, 0}) where {T}
-    computeTreeGraph(data, rn.child[]) |> rn.connect
+function computeTreeGraph(data::AbstractVector{T}, ln::LayerNode{T, <:Any, 0}) where {T}
+    ln.connect( computeTreeGraph(data, ln.child[]) ) + ln.bias
 end
 
 const symbolFromPar = symbolFrom∘idxSymOf
@@ -106,7 +74,7 @@ function genComputeGraph(parSet, par::ParamNode{T}) where {T}
         childNodes = map(dataOf(par)) do i
             genComputeGraph(parSet, i)
         end
-        LayerNode(par.lambda, childNodes, symbolFromPar(par))
+        LayerNode(par.lambda, childNodes, symbolFromPar(par), par.offset[])
     elseif sl == 1
         genComputeGraphCore(parSet, par)
     elseif sl == 2
