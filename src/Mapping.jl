a1628ca9d4e1b4a1086b2348137e437d0107680b
Weishi Wang <ws.frank.wang@gmail.com>
2024-11-12 01:44:18 -0500

    Added `VectorMemory` and udpated `ParamOperator`s.

--- src/Mapping.jl
+++ src/Mapping.jl
@@ -1,3 +1,34 @@
+struct VectorMemory{T, L} <: AbstractMemory{T, 1}
+    value::Memory{T}
+    shape::Val{L}
+
+    function VectorMemory(value::Memory{T}, ::Val{L}) where {T, L}
+        checkLength(value, :value, L)
+        new{T, L}(value, Val(L))
+    end
+end
+
+VectorMemory(input::AbstractArray) = VectorMemory(getMemory(input), Val(length(input)))
+
+VectorMemory(input::NonEmptyTuple{Any, L}) where {L} = 
+VectorMemory(getMemory(input), Val(L+1))
+
+VectorMemory(input::VectorMemory)  = itself(input)
+
+import Base: size, getindex, setindex!, iterate, length
+size(::VectorMemory{<:Any, L}) where {L} = (L,)
+
+getindex(arr::VectorMemory, i::Int) = getindex(arr.value, i)
+
+setindex!(arr::VectorMemory, val, i::Int) = setindex!(arr.value, val, i)
+
+iterate(arr::VectorMemory) = iterate(arr.value)
+iterate(arr::VectorMemory, state) = iterate(arr.value, state)
+
+length(::VectorMemory{<:Any, L}) where {L} = L
+
+const LinearMemory{T} = Union{Memory{T}, VectorMemory{T}}
+
 (po::ParamOperator)(input, param) = evalFunc(po, input, param)
 
 
@@ -8,6 +39,8 @@ struct PointerFunc{F<:Function, T<:NonEmptyTuple{Union{Int, Memory{Int}}}} <: Pa
 end
 
 evalFunc(f::PointerFunc, input, param) = f.apply(input, getindex.(Ref(param), f.pointer)...)
+
+const PointOneFunc{F} = PointerFunc{F, Tuple{Int}}
 
 
 struct OnlyInput{F<:Function} <: Evaluator{F}
@@ -30,40 +63,95 @@ end
 
 evalFunc(f::ParamFunc, input, param) = f.f(input, param)
 
-
-struct JoinParallel{C<:ParamOperator, F<:ParamOperator, J<:Function} <: ChainOperator
-    chain::C
-    apply::F
+struct PairCombine{J<:Function, FL<:ParamOperator, FR<:ParamOperator} <: ChainedOperator{J}
     joint::J
+    left::FL
+    right::FR
 end
 
-const AddParallel{L, R} = JoinParallel{L, R, typeof(+)}
-const MulParallel{L, R} = JoinParallel{L, R, typeof(*)}
+const AddPair{FL, FR} = PairCombine{typeof(+), FL, FR}
+const MulPair{FL, FR} = PairCombine{typeof(*), FL, FR}
 
-evalFunc(f::JoinParallel, input, param) = 
-f.joint(f.chain(input, param), f.apply(input, param))
+PairCombine(joint::F) where {F<:Function} = 
+(left::ParamOperator, right::ParamOperator) -> PairCombine(joint, left, right)
 
-JoinParallel(joint::Function) =  (x, y)->JoinParallel(x, y, joint)
+evalFunc(f::PairCombine, input, param) = 
+f.joint( f.left(input, param), f.right(input, param) )
 
-struct InsertOnward{C<:ParamOperator, F<:ParamOperator} <: ChainOperator
+#! Test!!
+struct ChainReduce{J<:Function, C<:LinearMemory{<:ParamOperator}} <: ChainedOperator{J}
+    joint::J
     chain::C
-    apply::F
+
+    function ChainReduce(joint::J, chain::C) where {J, C<:VectorMemory{<:ParamOperator}}
+        checkEmptiness(chain, :chain)
+        new{J, C}(joint, chain)
+    end
+
+    function ChainReduce(joint::J, chain::C) where {J, C<:AbstractVector{<:ParamOperator}}
+        checkEmptiness(chain, :chain)
+        new{J, C}(joint, getMemory(chain))
+    end
 end
 
-evalFunc(f::InsertOnward, input, param) = f.apply(f.chain(input, param), param)
+ChainReduce(joint::Function, chain::NonEmptyTuple{ParamOperator}) = 
+ChainReduce(joint, VectorMemory(chain))
+
+ChainReduce(joint::F) where {F<:Function} = Base.Fix1(ChainReduce, joint)
+
+evalFunc(f::ChainReduce, input, param) = 
+mapreduce(o->o(input, param), f.joint, f.chain.value)
+
+const AddChain{C} = ChainReduce{typeof(+), C}
+const MulChain{C} = ChainReduce{typeof(*), C}
 
 
-struct InsertInward{C<:ParamOperator, F<:ParamOperator} <: ChainOperator
-    chain::C
-    apply::F
+# struct JoinParallel{J<:Function, 
+#                     C<:Union{Memory{<:ParamOperator}, JoinParallel}} <: ChainedOperator
+#     joint::J
+#     chain::C
+
+#     # JoinParallel(joint::J, chain::C) where {J<:Function, C<:NonEmptyTuple{ParamOperator}} = 
+#     # new{J, eltype(C), C}(joint, chain)
+
+#     function JoinParallel(joint::J, chain::C) where 
+#                          {J<:Function, O<:ParamOperator, 
+#                           C<:Union{Memory{O}, NonEmptyTuple{O}}}
+#         checkEmptiness(chain, :chain)
+#         new{J, O, C}(joint, chain)
+#     end
+# end
+
+# JoinParallel(joint::J, f::F) where {J<:Function, F<:Function} = JoinParallel(joint, (f,))
+
+# evalFunc(f::JoinParallel, input, param) = 
+# mapreduce(fNode->fNode(input, param), f.joint, f.chain)
+
+# JoinParallel(joint::Function) = Base.Fix1(JoinParallel, joint)
+
+# const AddParallel{O, C} = JoinParallel{typeof(+), O, C}
+# const MulParallel{O, C} = JoinParallel{typeof(*), O, C}
+
+
+struct InsertOnward{C<:ParamOperator, F<:ParamOperator} <: ParamOperator
+    apply::C
+    dress::F
 end
 
-evalFunc(f::InsertInward, input, param) = f.chain(f.apply(input, param), param)
+evalFunc(f::InsertOnward, input, param) = f.dress(f.apply(input, param), param)
 
 
-function evalFunc(func::F, r) where {F<:Function}
+struct InsertInward{C<:ParamOperator, F<:ParamOperator} <: ParamOperator
+    apply::C
+    dress::F
+end
+
+evalFunc(f::InsertInward, input, param) = f.apply(f.dress(input, param), param)
+
+
+function evalFunc(func::F, input) where {F<:Function}
     fCore, pars = unpackFunc(func)
-    fCore(r, map(obtain, pars))
+    fCore(input, map(obtain, pars))
 end
 
 
@@ -89,3 +177,14 @@ function unpackFunc!(::NotParamFunc, f::Function, paramSet::PBoxAbtArray)
     end
     PointerFunc(evalCore, ids, objectid(paramSet)), params
 end
+
+
+struct ReturnTyped{T, F<:Function} <: TaggedFunction
+    f::F
+
+    function ReturnTyped(f::F, ::Type{T}) where {F, T}
+        new{T, F}(f)
+    end
+end
+
+(f::ReturnTyped{F, T})(arg) where {F, T} = convert(T, f(arg))
