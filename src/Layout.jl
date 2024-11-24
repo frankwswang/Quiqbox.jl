facccc4f63c19ae4fc1305adb411e0d692f4900e
Weishi Wang <ws.frank.wang@gmail.com>
2024-11-24 14:36:39 -0500

    Removed `DataDim`, `IndexPointer`, and `FieldPointer`.

--- src/Layout.jl
+++ src/Layout.jl
@@ -1,83 +1,54 @@
+abstract type TensorPointer{T, N} <: CompositeFunction end
+
 struct TensorType{T, N} <: StructuredType
-    type::Type{T}
-    size::NTuple{N, Int}
+    TensorType(::Type{<:AbstractArray{T, N}}) where {T, N} = new{T, N}()
+    TensorType(::Type{TensorType{T, N}}) where {T, N} = new{T, N}()
+    TensorType(::Type{T}) where {T} = new{T, 0}()
 end
 
-TensorType(::Type{T}) where {T} = TensorType(T, ())
+TensorType(::Type{<:ElementalParam{T}}) where {T} = TensorType(AbstractArray{T, 0})
 
-TensorType(arr::AbstractArray{T}) where {T} = TensorType(T, size(arr))
+TensorType(::Type{<:FlattenedParam{T, N}}) where {T, N} = TensorType(AbstractArray{T, N})
+
+TensorType(::Type{<:JaggedParam{T, N, O}}) where {T, N, O} = 
+TensorType(AbstractArray{AbstractArray{T, N}, O})
 
 TensorType(::T) where {T} = TensorType(T)
 
-abstract type TensorPointer{T} end
 
-abstract type SymbolPointer end
+struct FirstIndex <: StructuredType end
 
-struct Data0D end
-struct Data1D end
-struct Data2D end
-struct DataXD end
-
-const DataDim = Union{Data0D, Data1D, Data2D, DataXD}
-
-getDataDim(::Type{<:ElementalParam}) = Data0D()
-getDataDim(::Type{<:FlattenedParam}) = Data1D()
-getDataDim(::Type{<:JaggedParam}) = Data2D()
-getDataDim(::Type{<:Any}) = DataXD()
-getDataDim(::T) where {T<:JaggedParam} = getDataDim(T)
+const GeneralIndex = Union{Int, Symbol, FirstIndex}
 
 
-struct IndexPointer{T<:DataDim} <: CompositeFunction
-    idx::Int
-    dim::T
-end
-
-const GetIndex = IndexPointer{DataXD}
-
-IndexPointer(idx::Int, ele) = IndexPointer(idx, getDataDim(ele))
-IndexPointer(idx::Int) = IndexPointer(idx, DataXD())
-
-const IntOrSym = Union{Int, Symbol}
-
-struct FieldPointer{T<:TensorType, C<:IntOrSym} <: TensorPointer{T}
-    entry::C
-    type::T
-end
-
-FieldPointer(entry::IntOrSym, objOrType=Any) = FieldPointer(entry, TensorType(objOrType))
-
-struct ChainPointer{T<:TensorType, C<:Tuple{Missing, Vararg{IntOrSym}}} <: TensorPointer{T}
+struct ChainPointer{T, N, C<:Tuple{Vararg{GeneralIndex}}} <: TensorPointer{T, N}
     chain::C
-    type::T
+    type::TensorType{T, N}
 end
 
-ChainPointer(objOrType::Any=Any) = 
-ChainPointer((missing,), TensorType(objOrType))
+const IndexPointer{T, N} = ChainPointer{T, N, Tuple{Int}}
 
-ChainPointer(entry::IntOrSym, type::TensorType=TensorType(Any)) = 
-ChainPointer((missing, entry), type)
+ChainPointer(sourceType::TensorType=TensorType(Any)) = ChainPointer((), sourceType)
 
-ChainPointer(prev::FieldPointer, here::FieldPointer) = 
-ChainPointer((missing, prev.entry, here.entry), here.type)
+ChainPointer(entry::GeneralIndex, source::Any=Any) = 
+ChainPointer((entry,), TensorType(source))
 
-ChainPointer(prev::FieldPointer, here::ChainPointer) = 
-ChainPointer((missing, prev.entry, Base.tail(here.chain)...), here.type)
-
-ChainPointer(prev::ChainPointer, here::FieldPointer) = 
-ChainPointer((prev.chain..., here.entry), here.type)
-
-ChainPointer(prev::ChainPointer, here::ChainPointer) = 
-ChainPointer((prev.chain..., Base.tail(here.chain)...), here.type)
-
-ChainPointer(prev::IntOrSym, here::Union{FieldPointer, ChainPointer}) = 
+ChainPointer(prev::GeneralIndex, here::ChainPointer) = 
 ChainPointer(ChainPointer(prev), here)
 
-getField(obj, ::Missing) = itself(obj)
+ChainPointer(prev::ChainPointer, here::ChainPointer) = 
+ChainPointer((prev.chain..., here.chain...), here.type)
+
+
+getField(obj, ::FirstIndex) = first(obj)
 
 getField(obj, entry::Symbol) = getfield(obj, entry)
 
 getField(obj, entry::Int) = getindex(obj, entry)
 
-getField(obj, ptr::FieldPointer) = getField(obj, ptr.entry)
-
 getField(obj, ptr::ChainPointer) = foldl(getField, ptr.chain, init=obj)
+
+getField(ptr) = Base.Fix2(getField, ptr)
+
+
+const PointToField{T, N, C} = Base.Fix2{typeof(getField), ChainPointer{T, N, C}}
