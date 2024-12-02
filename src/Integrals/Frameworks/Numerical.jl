53f7815ee0c5f4c7bc7a4dfbc2a84aa57b0ba5d9
Weishi Wang <ws.frank.wang@gmail.com>
2024-12-01 21:02:48 -0500

    Optimized `numericalOneBodyInt` by introducing `composeOneBodyKernel`.

--- src/Integrals/Frameworks/Numerical.jl
+++ src/Integrals/Frameworks/Numerical.jl
@@ -17,15 +17,36 @@ function numericalIntegration(integrand::ReturnTyped{T},
 end
 
 
-numericalOneBodyInt(op::F, orb::EvalDimensionalKernel{T, D}, 
-                    pValDict::ParamValOrDict{T}) where {F<:DirectOperator, T, D} = 
-numericalOneBodyInt(op, (orb, orb), (pValDict, pValDict))
+function composeOneBodyKernel(op::O, ::Type{T}, termL::F1, termR::F2) where 
+                             {O<:Function, T, F1<:Function, F2<:Function}
+    PairCombine(StableBinary(*, T), adjoint∘termL, op(termR))
+end
+
+function composeOneBodyKernel(op::O, ::Type{T}, term::F) where {O<:Function, T, F<:Function}
+    composeOneBodyKernel(op, T, term, term)
+end
+
+function composeOneBodyKernel(::Identity, ::Type{T}, term::F) where {T, F<:Function}
+    function (input::I) where {I}
+        val = term(input)::T
+        val' * val
+    end
+end
+
+function numericalOneBodyInt(op::F, (orb,)::Tuple{EvalDimensionalKernel{T, D}}, 
+                             (pValDict,)::Tuple{ParamValOrDict{T}}) where 
+                            {F<:DirectOperator, T, D}
+    term = Base.Fix2(orb, pValDict)
+    integrand = changeFuncRange(composeOneBodyKernel(op, T, term), T)
+    bound = ntuple(_->one(T), Val(D))
+    numericalIntegration(integrand, (.-(bound), bound))
+end
 
 function numericalOneBodyInt(op::F, orbs::NTuple{2, EvalDimensionalKernel{T, D}}, 
                              pValGroups::NTuple{2, ParamValOrDict{T}}) where 
                             {F<:DirectOperator, T, D}
     termL, termR = Base.Fix2.(orbs, pValGroups)
-    integrand = changeFuncRange(PairCombine(StableBinary(*, T), termL, op(termR)), T)
+    integrand = changeFuncRange(composeOneBodyKernel(op, T, termL, termR), T)
     bound = ntuple(_->one(T), Val(D))
     numericalIntegration(integrand, (.-(bound), bound))
 end
