034dd19dee3ae9a93b0180b1d2559e576fbc2388
Weishi Wang <41162655+frankwswang@users.noreply.github.com>
2025-01-15 23:22:07 -0500

    Reorganized and optimized core functions.

--- src/Integrals/Engines/GaussianOrbitals.jl
+++ src/Integrals/Engines/GaussianOrbitals.jl
@@ -21,82 +21,93 @@ function polyGaussFuncSquaredNorm(α::T, degree::Int) where {T<:Real}
 end
 
 
-struct XpnPair{T<:Real}
-    left::T
-    right::T
-    sum::T
-    prod::T
-
-    function XpnPair(l::T, r::T) where {T}
-        checkPositivity(l)
-        checkPositivity(r)
-        new{T}(l, r, l+r, l*r)
-    end
+function gaussProdCore1(dxLR::T, xpnProdOverSum::T) where {T<:Real}
+    exp(- xpnProdOverSum * dxLR^2)
 end
 
-
-struct CenPair{T<:Real, D}
-    left::NTuple{D, T}
-    right::NTuple{D, T}
-    dist::T
-
-    CenPair(l::NonEmptyTuple{T, D}, r::NonEmptyTuple{T, D}) where {T, D} = 
-    new{T, D+1}(l, r, norm(l .- r))
-end
-
-function (cPair::CenPair{T})(xPair::XpnPair{T}) where {T}
-    (xPair.left .* cPair.left .+ xPair.right .* cPair.right) ./ xPair.sum
-end
-
-
-struct AngPair{D}
-    left::NTuple{D, Int}
-    right::NTuple{D, Int}
-    sum::NTuple{D, Int}
-
-    AngPair(l::NonEmptyTuple{Int, D}, r::NonEmptyTuple{Int, D}) where {D} = 
-    new{D+1}(l, r, l .+ r)
-end
-
-
-function gaussProdCore1(cPair::CenPair{T}, xPair::XpnPair{T}) where {T<:Real}
-    if iszero(cPair.dist)
-        T(1)
-    else
-        exp(- xPair.prod / xPair.sum * cPair.dist^2)
-    end
-end
-
-function gaussProdCore2(x1::T, x2::T, x::T, lx1::Int, lx2::Int, lx::Int) where {T<:Real}
-    lb = max(-lx,  lx - 2lx2)
-    ub = min( lx, 2lx1 - lx )
+function gaussProdCore2(dxML::T, dxMR::T, lxL::Int, lxR::Int, lx::Int) where {T<:Real}
+    lb = max(-lx,  lx - 2lxR)
+    ub = min( lx, 2lxL - lx )
     res = zero(T)
     for q in lb:2:ub
-        i = (lx + q) ÷ 2
-        j = (lx - q) ÷ 2
-        res += binomial(lx1,  i) * binomial(lx2,  j) * (x - x1)^(lx1 -i) * (x - x2)^(lx2 -j)
+        i = (lx + q) >> 1
+        j = (lx - q) >> 1
+        res += binomial(lxL, i) * binomial(lxR, j) * dxML^(lxL - i) * dxMR^(lxR - j)
+    end
+    res
+end
+
+function gaussProdCore2(lxL::Int, lxR::Int, lx::Int)
+    lb = max(-lx,  lx - 2lxR)
+    ub = min( lx, 2lxL - lx )
+    res = zero(Int)
+    for q in lb:2:ub
+        res += Int(isequal(2lxL, lx+q) * isequal(2lxR, lx-q))
     end
     res
 end
 
 
-function overlapPGTO(cPair::CenPair{T, D}, xPair::XpnPair{T}, 
-                     aPair::AngPair{D}) where {T, D}
-    α = xPair.sum
-    mapreduce(*, cPair.left, cPair.right, cPair(xPair), aPair.left, aPair.right, 
-                 aPair.sum) do x1, x2, x, i1, i2, i
-        mapreduce(+, 0:(i÷2)) do j
-            gaussProdCore2(x1, x2, x, i1, i2, 2j) * polyGaussFuncSquaredNorm(α/2, j)
-        end
-    end * gaussProdCore1(cPair, xPair)
+struct PrimGaussOrbData{T, D}
+    cen::NTuple{D, T}
+    xpn::T
+    ang::NTuple{D, Int}
+
+    PrimGaussOrbData(cen::NonEmptyTuple{T, D}, xpn::T, 
+                     ang::NonEmptyTuple{Int, D}) where {T, D} = 
+    new{T, D+1}(cen, xpn, ang)
 end
 
 
-function overlapPGTO(xpn::T, ang::NTuple{D, Int}) where {T, D}
+struct GaussProductData{T, D}
+    lhs::PrimGaussOrbData{T, D}
+    rhs::PrimGaussOrbData{T, D}
+    cen::NTuple{D, T}
+    xpn::T
+
+    function GaussProductData(oData1::PrimGaussOrbData{T, D}, 
+                              oData2::PrimGaussOrbData) where {T, D}
+        xpnSum = oData1.xpn + oData2.xpn
+        cenNew = (oData1.xpn .* oData1.cen .+ oData2.xpn .* oData2.cen) ./ xpnSum
+        new{T, D}(oData1, oData2, cenNew, xpnSum)
+    end
+end
+
+
+function overlapPGTO(data::GaussProductData{T}) where {T}
+    cenL = data.lhs.cen
+    cenR = data.rhs.cen
+    cenM = data.cen
+
+    xpn = data.xpn
+    xpnRatio = data.lhs.xpn * data.rhs.xpn / xpn
+
+    angL = data.lhs.ang
+    angR = data.rhs.ang
+
+    mapreduce(*, cenL, cenR, cenM, angL, angR) do xL, xR, xM, iL, iR
+        dxLR = xL - xR
+        jRange = 0:((iL + iR) ÷ 2)
+        if isequal(dxLR, zero(T))
+            mapreduce(+, jRange) do j
+                gaussProdCore2(iL, iR, 2j) * polyGaussFuncSquaredNorm(xpn/2, j)
+            end
+        else
+            mapreduce(+, jRange) do j
+                xML = xM - xL
+                xMR = xM - xR
+                gaussProdCore2(xML, xMR, iL, iR, 2j) * polyGaussFuncSquaredNorm(xpn/2, j)
+            end * gaussProdCore1(dxLR, xpnRatio)
+        end
+    end
+end
+
+function overlapPGTO(xpn::T, ang::NonEmptyTuple{Int}) where {T}
     mapreduce(*, ang) do i
         polyGaussFuncSquaredNorm(xpn, i)
     end
 end
+
 
 function getAngularNums(orb::PrimATOcore)
     angFunc = orb.f.apply.f.right.f
@@ -112,12 +123,14 @@ function getCenCoordPtr(orb::PrimitiveOrbCore)
     orb.f.dress.select
 end
 
+
 function preparePGTOconfig(orb::PrimGTOcore)
     cenIds = getCenCoordPtr(orb)
     xpnIdx = getExponentPtr(orb)
     ang = getAngularNums(orb)
     cenIds, xpnIdx, ang
 end
+
 
 struct OverlapGTOrbSelf{T, D} <: OrbitalIntegrator{T, D}
     xpn::FlatPSetInnerPtr{T}
@@ -129,10 +142,6 @@ function (f::OverlapGTOrbSelf{T})(pars::FilteredVecOfArr{T}) where {T}
     overlapPGTO(xpnVal, f.ang)
 end
 
-function genGTOrbOverlapFunc((orb,)::Tuple{PrimGTOcore{T, D}}) where {T, D}
-    _, xpnIdx, ang = preparePGTOconfig(orb)
-    OverlapGTOrbSelf(xpnIdx, ang)
-end
 
 struct OverlapGTOrbPair{T, D} <: OrbitalIntegrator{T, D}
     cen::NTuple{2, NTuple{ D, FlatPSetInnerPtr{T} }}
@@ -142,16 +151,24 @@ end
 
 function (f::OverlapGTOrbPair{T})(pars1::FilteredVecOfArr{T}, 
                                   pars2::FilteredVecOfArr{T}) where {T}
-    cen1 = getField(pars1, f.cen[1])
-    cen2 = getField(pars2, f.cen[2])
-    xpn1 = getField(pars1, f.xpn[1])
-    xpn2 = getField(pars2, f.xpn[2])
-    overlapPGTO(CenPair(cen1, cen2), XpnPair(xpn1, xpn2), AngPair(f.ang...))
+    c1 = getField(pars1, first(f.cen))
+    x1 = getField(pars1, first(f.xpn))
+    d1 = PrimGaussOrbData(c1, x1, first(f.ang))
+    c2 = getField(pars2,  last(f.cen))
+    x2 = getField(pars2,  last(f.xpn))
+    d2 = PrimGaussOrbData(c2, x2,  last(f.ang))
+    GaussProductData(d1, d2) |> overlapPGTO
 end
+
 
 function genGTOrbOverlapFunc(orbs::NTuple{2, PrimGTOcore{T, D}}) where {T, D}
     configs = preparePGTOconfig.(orbs)
     OverlapGTOrbPair(getindex.(configs, 1), getindex.(configs, 2), getindex.(configs, 3))
+end
+
+function genGTOrbOverlapFunc((orb,)::Tuple{PrimGTOcore{T, D}}) where {T, D}
+    _, xpnIdx, ang = preparePGTOconfig(orb)
+    OverlapGTOrbSelf(xpnIdx, ang)
 end
 
 
