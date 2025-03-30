fcab6372ddd10d1aab8d6c0189856e50fd6995a4
Weishi Wang <41162655+frankwswang@users.noreply.github.com>
2025-03-30 14:04:48 -0400

    Removed obsolete type tests.

--- test/unit-tests/Types-test.jl
+++ test/unit-tests/Types-test.jl
@@ -3,30 +3,6 @@ using Quiqbox
 
 @testset "Types.jl" begin
 
-@test Quiqbox.FlatParamVec <: Quiqbox.MiscParamVec
-
-@test !(Quiqbox.MiscParamSet <: Quiqbox.MiscParamVec)
-
-@test !(Quiqbox.FlatParamSet <: Quiqbox.MiscParamVec)
-
-@test !(Quiqbox.FlatParamSet <: Quiqbox.FlatParamVec)
-
-@test Quiqbox.TypedFlatParamSet <: Quiqbox.FlatParamVec
-
-@test Quiqbox.TypedFlatParamSet <: Quiqbox.FlatParamSet
-
-@test Quiqbox.PrimitiveParamSet <: Quiqbox.TypedFlatParamSet
-
-@test Quiqbox.TypedMiscParamSet <: Quiqbox.MiscParamVec
-
-@test Quiqbox.TypedMiscParamSet <: Quiqbox.MiscParamSet
-
-@test Quiqbox.FlatParamSet <: Quiqbox.AbstractFlatParamSet
-
-@test Quiqbox.FlatParamSetMixedVec <: Quiqbox.AbstractMiscParamSet
-
-@test Quiqbox.MiscParamSet <: Quiqbox.AbstractMiscParamSet
-
 @test Quiqbox.PrimGTO <: Quiqbox.PrimitiveOrb
 
 @test Quiqbox.EvalPrimOrb <: Quiqbox.ScaledOrbital
@@ -46,12 +22,5 @@ using Quiqbox
 @test Quiqbox.FPrimGTO <: Quiqbox.FrameworkOrb
 
 @test Quiqbox.FCompGTO <: Quiqbox.FrameworkOrb
-
-@test eltype( Quiqbox.OrbCoreDataSeq{Int, 2, Quiqbox.PrimitiveOrbCore{Int, 2}, 
-              Vector{Quiqbox.ShapedMemory{Int, 1}}} ) <: Quiqbox.OrbCoreData
-
-@test Quiqbox.FilteredFlatParamSet <: Quiqbox.Filtered
-
-@test Quiqbox.TypedParamInput <: Quiqbox.Filtered
 
 end
