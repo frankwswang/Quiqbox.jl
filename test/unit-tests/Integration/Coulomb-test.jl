eabe6f5b07a4ba0ecd8525362ff2468932bde095
Weishi Wang <41162655+frankwswang@users.noreply.github.com>
2025-07-12 00:06:02 -0400

    Formatted the tests.

--- test/unit-tests/Integration/Coulomb-test.jl
+++ test/unit-tests/Integration/Coulomb-test.jl
@@ -35,30 +35,32 @@ nucInfo1 = 1 => (0., 0., 0.)
 @test computePGTOrbPointCoulombField(nucInfo1, gtoProd63) ==
       computePGTOrbPointCoulombField(nucInfo1, gtoProd36) ≈ -0.11995218441914622
 
+
 gtoProd14 = GaussProductInfo((gtoData1, gtoData4))
 gtoProd41 = GaussProductInfo((gtoData4, gtoData1))
 gtoProd78 = GaussProductInfo((gtoData7, gtoData8))
 gtoProd87 = GaussProductInfo((gtoData8, gtoData7))
 
-computePGTOrbTwoBodyRepulsion(gtoProd11, gtoProd22) ≈ 
-computePGTOrbTwoBodyRepulsion(gtoProd22, gtoProd11) ≈ 
-1.7675350484831864e-6
-computePGTOrbTwoBodyRepulsion(gtoProd12, gtoProd12) ≈ 
-computePGTOrbTwoBodyRepulsion(gtoProd12, gtoProd21) ≈ 
-computePGTOrbTwoBodyRepulsion(gtoProd21, gtoProd21) ≈ 
-6.267963629018787e-8
+@test computePGTOrbTwoBodyRepulsion(gtoProd11, gtoProd22) ≈ 
+      computePGTOrbTwoBodyRepulsion(gtoProd22, gtoProd11) ≈ 
+      1.7675350484831864e-6
 
-computePGTOrbTwoBodyRepulsion(gtoProd33, gtoProd44) ≈ 
-computePGTOrbTwoBodyRepulsion(gtoProd44, gtoProd33) ≈ 
-0.7291219052871128
+@test computePGTOrbTwoBodyRepulsion(gtoProd12, gtoProd12) ≈ 
+      computePGTOrbTwoBodyRepulsion(gtoProd12, gtoProd21) ≈ 
+      computePGTOrbTwoBodyRepulsion(gtoProd21, gtoProd21) ≈ 
+      6.267963629018787e-8
 
-computePGTOrbTwoBodyRepulsion(gtoProd14, gtoProd78) ≈ 
-computePGTOrbTwoBodyRepulsion(gtoProd41, gtoProd78) ≈ 
-computePGTOrbTwoBodyRepulsion(gtoProd41, gtoProd87) ≈ 
-computePGTOrbTwoBodyRepulsion(gtoProd14, gtoProd87) ≈ 
-computePGTOrbTwoBodyRepulsion(gtoProd78, gtoProd14) ≈ 
-computePGTOrbTwoBodyRepulsion(gtoProd87, gtoProd14) ≈ 
-computePGTOrbTwoBodyRepulsion(gtoProd87, gtoProd41) ≈ 
-computePGTOrbTwoBodyRepulsion(gtoProd78, gtoProd41) ≈ -2.4175946692430508e-9
+@test computePGTOrbTwoBodyRepulsion(gtoProd33, gtoProd44) ≈ 
+      computePGTOrbTwoBodyRepulsion(gtoProd44, gtoProd33) ≈ 
+      0.7291219052871128
+
+@test computePGTOrbTwoBodyRepulsion(gtoProd14, gtoProd78) ≈ 
+      computePGTOrbTwoBodyRepulsion(gtoProd41, gtoProd78) ≈ 
+      computePGTOrbTwoBodyRepulsion(gtoProd41, gtoProd87) ≈ 
+      computePGTOrbTwoBodyRepulsion(gtoProd14, gtoProd87) ≈ 
+      computePGTOrbTwoBodyRepulsion(gtoProd78, gtoProd14) ≈ 
+      computePGTOrbTwoBodyRepulsion(gtoProd87, gtoProd14) ≈ 
+      computePGTOrbTwoBodyRepulsion(gtoProd87, gtoProd41) ≈ 
+      computePGTOrbTwoBodyRepulsion(gtoProd78, gtoProd41) ≈ -2.4175946692430508e-9
 
 end
