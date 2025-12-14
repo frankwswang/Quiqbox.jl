d908ff485f8e1bd13880f94686887924131d412e
Weishi Wang <41162655+frankwswang@users.noreply.github.com>
2025-12-14 13:49:49 -0500

    Removed `checkFuncReturn` to avoid `Base.return_types` and made all check functions return something other than `nothing`.

--- src/Exception.jl
+++ src/Exception.jl
@@ -1,17 +1,10 @@
-function checkFuncReturn(f::F, fName::Symbol, ::Type{TArg}, ::Type{TReturn}) where 
-                        {F<:Function, TArg, TReturn}
-    Base.return_types(f, (TArg,))[] == TReturn || 
-    throw(ArgumentError("`$fName`: `$f` should return a `$TReturn` given an input "*
-                        "argument of type `$TArg`."))
-end
-
-
 function checkCollectionMinLen(data, dataSym::Symbol, minLen::Int)
     minLen = abs(minLen)
     str = minlen > 1 ? "s." : "."
     dataLen = length(data)
     dataLen < minLen && 
     throw(ArgumentError("`$dataSym`: $data should contain at least $minLen element"* str))
+
     dataLen == minLen
 end
 
@@ -20,6 +13,7 @@ function checkEmptiness(obj, name::Symbol; reverseCheck::Bool=false)
     if ifelse(reverseCheck, !, itself)(obj|>isempty)
         throw(AssertionError("`$name` must$str be empty."))
     end
+
     length(obj)
 end
 
@@ -29,22 +23,27 @@ function checkLengthCore(objLen::Int, objName::Symbol,
         str = ismissing(lenName) ? "be equal to $len" : "match $lenName: $len"
         throw(AssertionError("The length of `$objName` must " * str * "."))
     end
-    nothing
+
+    objLen
 end
 
 function checkLength(obj, name::Symbol, len::Int, lenName::Union{Missing, String}=missing)
     checkLengthCore(length(obj), name, len, lenName)
 end
-#! Standardize return type
+
 function checkPositivity(num::Real, allowZero::Bool=false)
     subStr = ifelse(allowZero, "non-negative", "positive")
     (num + Int(allowZero)) > 0 || throw(AssertionError("`num` should be $subStr."))
-    nothing
+
+    num
 end
 
 function checkBottomType(::Type{T}) where {T}
-    T <: Union{} && throw(AssertionError("`T` must not be `Union{}`."))
-    nothing
+    if T <: Union{}
+        throw(AssertionError("`T` must not be `Union{}`."))
+    end
+
+    T
 end
 
 
@@ -73,5 +72,6 @@ function checkBottomArray(arr::AbstractArray)
     if eltype(arr) <:Union{} && !isempty(arr)
         throw(AssertionError("`arr` must be empty if its element type is `Union{}`."))
     end
-    nothing
+
+    arr
 end
